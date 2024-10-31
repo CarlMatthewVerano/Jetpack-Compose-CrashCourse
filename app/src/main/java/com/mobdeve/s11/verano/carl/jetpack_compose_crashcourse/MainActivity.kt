8ccb9a67222ab13227d12eaf6c65b2101cc18cf7
CarlMatthewVerano <carlmatthewverano@gmail.com>
2024-10-31 22:45:30 +0800

    feat: Different Layouts in Compose

--- app/src/main/java/com/mobdeve/s11/verano/carl/jetpack_compose_crashcourse/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/verano/carl/jetpack_compose_crashcourse/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mobdeve.s11.verano.carl.jetpack_compose_crashcourse.ui.theme.JetpackComposeCrashCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +43,43 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    // Row - horizontal arrangement; linear layout with horizontal orientation
+    // Column - vertical arrangement; linear layout with vertical orientation
+    Box ( // equivalent to frame layout
+//        horizontalAlignment = Alignment.CenterHorizontally, // only works for Column
+//        verticalArrangement = Arrangement.Center, // only works for Column
+
+        contentAlignment = Alignment.Center, // works for both Column and Row
         modifier = modifier
-    )
+            .background(Color.Yellow)
+            .size(400.dp)
+            // .fillMaxSize() // fill the entire screen
+            // fillMaxWidth() - fill the entire width
+            // fillMaxHeight() - fill the entire height
+            // size(100.dp) - set the size of the element; 100x100 dp
+
+    ) {
+        Text(
+            text = "Hello $name!!",
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+            modifier = modifier
+//                .fillMaxWidth() // fill the entire width of the parent
+                .align(Alignment.BottomEnd)
+
+        )
+        Text(
+            text = "WIGGAasdasdasdas!",
+            color = Color.Red,
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic,
+
+            modifier = modifier
+
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
